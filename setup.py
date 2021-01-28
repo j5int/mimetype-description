791eaa49054db0ca580108da5f9627eef77f1b51
David Fraser <davidfraser@users.noreply.github.com>
2021-01-28 15:02:55 +0200

    But don't include a >= 3.7 requirement
    Including 3.7 as a minimum excludes 2.7...

--- setup.py
+++ setup.py
@@ -22,5 +22,5 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*,!=3.5.*,!=3.6.*,>=3.7"
+    python_requires=">=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*,!=3.5.*,!=3.6.*"
 )
